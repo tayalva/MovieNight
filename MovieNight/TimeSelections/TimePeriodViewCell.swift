cf85a4fd5e9db51f5ad4b8836103ec26d92c2b89
tayalva <tay.smith@me.com>
2017-10-23 09:11:10 -0700

    added 3rd option of time period selection

---
+++ MovieNight/TimeSelections/TimePeriodViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TimePeriodViewCell.swift
+//  MovieNight
+//
+//  Created by Taylor Smith on 10/23/17.
+//  Copyright © 2017 Taylor Smith. All rights reserved.
+//
+
+import UIKit
+
+class TimePeriodViewCell: UITableViewCell {
+
+    @IBOutlet weak var selectionMark: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MovieNight/TimeSelections/TimePeriodViewController.swift
+++ MovieNight/TimeSelections/TimePeriodViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class TimePeriodViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var timePeriodArray: [String] = ["50's", "60's", "70's", "80's", "90's", "2000's", "2010's"]
+    var timePeriodArray: [String] = ["90's", "2000's", "2010's", "Now Playing"]
+    var maxSelection = 1
+    var timeSelection = ""
+    
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var finishButton: UIButton!
     
     
     override func viewDidLoad() {
@@ -37,6 +41,34 @@ class TimePeriodViewController: UIViewController, UITableViewDelegate, UITableVi
         
         return cell
         
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! TimePeriodViewCell
+        print(indexPath)
+        
+        if cell.selectionMark.image == #imageLiteral(resourceName: "EmptySelection") && maxSelection != 0 {
+            
+            cell.selectionMark.image = #imageLiteral(resourceName: "SelectedCircle")
+            maxSelection -= 1
+            timeSelection = timePeriodArray[indexPath.row]
+            
+        } else if cell.selectionMark.image == #imageLiteral(resourceName: "SelectedCircle") {
+            
+            cell.selectionMark.image = #imageLiteral(resourceName: "EmptySelection")
+            maxSelection += 1
+            timeSelection = ""
+        }
+        
+        if maxSelection == 0 {
+            finishButton.alpha = 1.0
+            finishButton.isEnabled = true
+        } else if maxSelection != 0 {
+            finishButton.alpha = 0.30
+            finishButton.isEnabled = false
+        }
+        
+        print(timeSelection)
     }
     
 
